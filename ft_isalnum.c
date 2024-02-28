bf5fcbf31bd397113a452465e84b68e5b12d8bf0
Illia Tykhonov <itykhono@c4r1s2.42warsaw.pl>
2024-02-28 12:15:51 +0100

    ft_isalnum is done

---
+++ ft_isalnum.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isalnum.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: itykhono <itykhono@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/27 14:46:30 by itykhono          #+#    #+#             */
+/*   Updated: 2024/02/28 12:14:30 by itykhono         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_isalnum(int some_val)
+{
+	if ((ft_isalpha(some_val) == 1)
+		|| (ft_isdigit(some_val) == 1))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
+/*
+int main()
+{
+	int a = 65;
+	printf("%i", ft_isalnum(a));
+}
+*/
