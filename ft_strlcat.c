6c2630b4367707f8e89ff6263ba8519fc9516cd6
Illia Tykhonov <itykhono@c1r7s1.42warsaw.pl>
2024-03-07 12:44:52 +0100

    strlcat + norms

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: itykhono <itykhono@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 16:40:19 by itykhono          #+#    #+#             */
-/*   Updated: 2024/03/07 12:29:26 by itykhono         ###   ########.fr       */
+/*   Updated: 2024/03/07 12:44:26 by itykhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,17 +23,15 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	local_size = 0;
 	if (size <= 1 || size < dst_len)
 		return (ft_strlen(src) + size);
-		
 	while (src[local_size] != '\0' && local_size < size - 1)
 	{
 		dst[dst_dynamiclen] = src[local_size];
 		local_size++;
 		dst_dynamiclen++;
 	}
-	dst[local_size] = '\0'; 
+	dst[local_size] = '\0';
 	if (size > dst_len)
 		return (dst_len + ft_strlen(src));
 	else
 		return (local_size + ft_strlen(src));
-		
 }
