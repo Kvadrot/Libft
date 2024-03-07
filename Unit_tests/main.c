adb1c48a0b50b0a260c1e045be497ea5e0170dca
Illia Tykhonov <itykhono@c1r7s1.42warsaw.pl>
2024-03-07 15:27:48 +0100

    ft_strchr + noms + tests

--- Unit_tests/main.c
+++ Unit_tests/main.c
@@ -16,4 +16,5 @@ int main(void) {
 	test_ft_strlcat();
 	test_ft_toupper();
 	test_ft_tolower();
+	test_ft_strchr();
 }
--- libft.h
+++ libft.h
@@ -20,6 +20,8 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 int	ft_toupper(int c);
 int ft_tolower(int c);
+char *ft_strchr(const char *s, int c);
+
 
 
 #endif
