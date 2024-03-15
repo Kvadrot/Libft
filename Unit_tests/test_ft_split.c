cfd82d525aaf11f83856e3fbe2a5d8789ec896d4
Illia Tykohonov <send.me.your.maild@gmail.com>
2024-03-15 18:11:03 +0100

    temp split

--- Unit_tests/test_ft_split.c
+++ Unit_tests/test_ft_split.c
@@ -3,15 +3,15 @@
 
 void test_ft_split()
 {
-	char *test = "0123|567|94";
+	char *test = "||";
 	char spaceChar = '|';
 	char **mainstr = ft_split(test, spaceChar);
 	if (!mainstr)
 		printf("gamno");
-	// else {
-	// 	for (size_t i = 0; i < strlen(*mainstr); i++)
-	// 	{
-	// 		printf("s = %s\n", mainstr[i]);
-	// 	}
-	// }
+	else {
+		for (size_t i = 0; mainstr[i] != NULL; i++)
+		{
+			printf("s = %s\n", mainstr[i]);
+		}
+	}
 }
--- ft_split.c
+++ ft_split.c
@@ -6,53 +6,74 @@
 /*   By: itykhono <itykhono@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 11:20:40 by itykhono          #+#    #+#             */
-/*   Updated: 2024/03/15 15:54:17 by itykhono         ###   ########.fr       */
+/*   Updated: 2024/03/15 18:07:25 by itykhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int	ft_get_words_amount(const char *source, char c)
+{
+	int result;
+	int		main_counter;
+	int		sub_counter;
+
+	result = 0;
+	main_counter = 0;
+	while (source[main_counter] != '\0')
+	{
+		while (source[main_counter] != '\0' && source[main_counter] == c)
+			main_counter++;
+		sub_counter = main_counter;
+		while (source[sub_counter] != '\0' && source[sub_counter] != c)
+			sub_counter++;
+		source += sub_counter;
+		main_counter = 0;
+		result++;
+		// printf("%d\n", result);
+	}
+	// printf("%d\n", result);
+	return (result);
+}
+
 char	**ft_split(char const *s, char c)
  {
-	// char	**result;
+	char	**result;
 	int		main_counter;
 	int		sub_counter;
 	char	*tempstr;
+	int		words_counter;
 
-	int test = 2;
 	if (!s)
 		return(NULL);
+	// if (ft_get_words_amount(s,c) == 0)
+	// {
+	// 	result = ((char **)malloc(1 * sizeof(char *)));
+	// 	if (!result)
+	// 		return (NULL);
+	// 	result[0] = NULL;
+	// }
+	result = (char **)ft_calloc(ft_get_words_amount(s,c) + 1, sizeof(char *));
 	main_counter = 0;
-
+	words_counter = 0;
 	while (s[main_counter] != '\0')
 	{
 		while (s[main_counter] != '\0' && s[main_counter] == c)
 			main_counter++;
 		sub_counter = main_counter;
-		printf("==============\n");
-		printf("s[sub] = %c\n", s[sub_counter]);
-		printf("==============\n");
-			
 		while (s[sub_counter] != '\0' && s[sub_counter] != c)
-		{
 			sub_counter++;
-		}
 		tempstr = ft_substr(s, main_counter, sub_counter - main_counter);
 		s += sub_counter;
+		printf("%s\n", tempstr);
+		if (!tempstr)
+			return (NULL);
+		result[words_counter] = tempstr;
+		words_counter++;
 		main_counter = 0;
-		// printf("startInd = %d\n", main_counter);
-		printf("len = %d\n",sub_counter - main_counter);
-		printf("tempstr = %s\n", tempstr);
-		// printf("main_counter = %c\n", s[main_counter]);
-		// printf("len = %zu\n", ft_strlen(s));
-		test--;
-		if( test == -1)
-		{
-			break;
-		}
 	}
-
-	return (NULL);
+	result[words_counter] = NULL;
+	return (result);
  }
 
  // "0123 567 94"
