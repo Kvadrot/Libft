ea322eb08fb0b7947c6f7d34447e524e8fa5a33f
Illia Tykohonov <send.me.your.maild@gmail.com>
2024-03-16 10:09:23 +0100

    split + norms TODO: 25 lines for func

--- ft_split.c
+++ ft_split.c
@@ -6,16 +6,31 @@
 /*   By: itykhono <itykhono@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/15 11:20:40 by itykhono          #+#    #+#             */
-/*   Updated: 2024/03/15 19:37:14 by itykhono         ###   ########.fr       */
+/*   Updated: 2024/03/16 10:08:40 by itykhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static void	ft_free_split(char **split_result)
+{
+	size_t	i;
+
+	if (!split_result)
+		return (NULL);
+	i = 0;
+	while (split_result[i] != NULL)
+	{
+		free(split_result[i]);
+		i++;
+	}
+	free(split_result);
+}
+
 static int	ft_get_words_amount(const char *source, char c)
 {
-	int 		result;
-	size_t		main_counter;
+	int		result;
+	size_t	main_counter;
 
 	result = 0;
 	main_counter = 0;
@@ -32,17 +47,16 @@ static int	ft_get_words_amount(const char *source, char c)
 }
 
 char	**ft_split(char const *s, char c)
- {
+{
 	char	**result;
-	int		main_counter;
-	int		sub_counter;
 	char	*tempstr;
 	int		words_counter;
+	int		main_counter;
+	int		sub_counter;
 
 	if (!s)
-		return(NULL);
-
-	result = (char **)ft_calloc(ft_get_words_amount(s,c) + 1, sizeof(char *));
+		return (NULL);
+	result = (char **)ft_calloc(ft_get_words_amount(s, c) + 1, sizeof(char *));
 	main_counter = 0;
 	words_counter = 0;
 	while (s[main_counter] != '\0')
@@ -50,18 +64,21 @@ char	**ft_split(char const *s, char c)
 		while (s[main_counter] != '\0' && s[main_counter] == c)
 			main_counter++;
 		if (s[main_counter] == '\0')
-			break;
+			break ;
 		sub_counter = main_counter;
 		while (s[sub_counter] != '\0' && s[sub_counter] != c)
 			sub_counter++;
 		tempstr = ft_substr(s, main_counter, sub_counter - main_counter);
 		s += sub_counter;
 		if (!tempstr)
+		{
+			ft_free_split(result);
 			return (NULL);
+		}
 		result[words_counter] = tempstr;
 		words_counter++;
 		main_counter = 0;
 	}
 	result[words_counter] = NULL;
 	return (result);
- }
+}
