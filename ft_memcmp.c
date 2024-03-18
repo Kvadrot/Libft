65cee3a3f7968d7860d31cab111db965d0496061
Illia Tykhonov <itykhono@c3r6s1.42warsaw.pl>
2024-03-18 19:36:22 +0100

    ft_memcmp.c + fix

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: itykhono <itykhono@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 13:51:01 by itykhono          #+#    #+#             */
-/*   Updated: 2024/03/11 12:22:44 by itykhono         ###   ########.fr       */
+/*   Updated: 2024/03/18 19:28:55 by itykhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,25 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	int		ind;
-	size_t	comparablesize;
+	size_t	ind;
 	int		s1len;
 	int		s2len;
 
 	ind = 0;
 	s1len = ft_strlen(s1);
 	s2len = ft_strlen(s2);
-	comparablesize = 0;
 	if (n == 0)
 		return (0);
 	while (((char *)s1)[ind] != '\0'
-		&& ((char *)s2)[ind] != '\0' && comparablesize <= n)
+		&& ((char *)s2)[ind] != '\0' && ind < n)
 	{
-		if (((char *)s1)[ind] > ((char *)s2)[ind])
-			return (s1 - s2);
-		else if (((char *)s1)[ind] < ((char *)s2)[ind])
-			return (s2 - s1);
+		if (((unsigned char *)s1)[ind] != ((unsigned char *)s2)[ind])
+			return (((unsigned char *)s1)[ind] - ((unsigned char *)s2)[ind]);
 		ind++;
 	}
-	if (s1len > s2len)
-		return (0 - ((int *)s2)[ind]);
-	else if (s1len < s2len)
-		return (0 - ((int *)s1)[ind]);
-	return (ind);
+	if (s1len > s2len && ind < n)
+		return (0 + ((unsigned char *)s1)[ind]);
+	else if (s1len < s2len && ind < n)
+		return (0 - ((unsigned char *)s2)[ind]);
+	return (0);
 }
