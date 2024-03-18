6c1120c39bba2a44627788ec993dcc922dc74bca
Illia Tykhonov <itykhono@c3r6s1.42warsaw.pl>
2024-03-18 20:08:13 +0100

    ft_strrchr + fix

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: itykhono <itykhono@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/07 20:34:04 by itykhono          #+#    #+#             */
-/*   Updated: 2024/03/12 15:43:16 by itykhono         ###   ########.fr       */
+/*   Updated: 2024/03/18 20:07:13 by itykhono         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,15 @@
 char	*ft_strrchr(const char *s, int c)
 {
 	char	search_char;
-	size_t	slen;
+	int		slen;
 
 	search_char = (char)c;
-	slen = ft_strlen(s) - 1;
+	slen = ft_strlen(s);
 	if (c == '\0')
 		return ((char *)&s[ft_strlen(s)]);
 	else
 	{
-		while (s[slen] > 0)
+		while (slen >= 0)
 		{
 			if (s[slen] == search_char)
 				return ((char *)&s[slen]);
